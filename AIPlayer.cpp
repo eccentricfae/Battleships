8571c9af20e8d535c890a5ccfe6be1375b83087d
Ria <adrianzabax@gmail.com>
2021-08-08 15:13:41 +0200

    Creating files for the new version

---
+++ AIPlayer.cpp
@@ -0,0 +1,3 @@
+#include "AIPlayer.h"
+
+//
---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-08-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World\n";
+
+    return 0;
+}
